980f1e9427136dd44205f805f1d5a9bbb9dc56f6
Michael Pascuzzi <gatorpazz@gmail.com>
2015-06-08 04:57:27 -0400

    Added the full reposTab feature, adjusted styling and JS

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ $('#about').click(function() {
   $('nav .tab').removeClass('active');
   $(this).addClass('active');
   $('.hide').removeClass('hidden');
+  $('.repositories').addClass('hidden');
 });
 
 $('#repos').click(function() {
   $('nav .tab').removeClass('active');
   $(this).addClass('active');
   $('.hide').addClass('hidden');
+  $('.repositories').removeClass('hidden');
 })
